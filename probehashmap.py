4b2fe92f4e351f72779ad3b61b66c854e8f74306
ajitluhach <ajit96singh@gmail.com>
2017-06-28 11:23:01 +0530

    Linear probing hash map

---
+++ probehashmap.py
@@ -0,0 +1,54 @@
+from hashmapbase import HashMapBase
+
+
+class ProbeHasMap(HashMapBase):
+    """Hash map implemented with linear probing for collision resolution."""
+    _AVAIL = object()  # sentinal marks locations of previous deletion
+
+    def _is_available(self, j):
+        """Return True if index j is available in table."""
+        return self._table[j] is None or self._table[j] is ProbeHasMap._AVAIL
+
+    def _find_slot(self, j, k):
+        """Search for k in bucket at index j.
+
+        Return (success, index) tuple, described as follows:
+        If match was found, success is True and index denotes its location.
+        If no match found, success is False and index denotes first available
+        slot.
+        """
+        firstAvail = None
+        while True:  # loop forever till key found
+            if self._is_available(j):
+                if firstAvail is None:
+                    firstAvail = j  # mark this as firstavail, empty slot
+                if self._table[j] is None:  # if bucket is None
+                    return (False, firstAvail)  # return false
+            elif k == self._table[j]._key:  # found a match
+                return (True, j)  # return true
+            j = (j + 1) % len(self._table)  # keep looking vertically
+
+    def _bucket_getitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError('Key Error: ' + repr(k))
+        return self._table[s]._value
+
+    def _bucket_setitem(self, j, k, v):
+        found, s = self._find_slot(j, k)
+        if not found:
+            self._table[s] = self._Item(k, v)
+            self._n += 1
+        else:
+            self._table[s]._value = v
+
+    def _bucket_delitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError('Key Error: ', repr(k))
+        self._table[s] = ProbeHasMap._AVAIL
+
+    def __iter__(self):
+        for j in range(len(self._table)):
+            if not self._is_available(j):
+                yield self._table[j]._key
