65a2109a42d7d3eaff575fe62b330e9f53c56e11
ajitluhach <ajit96singh@gmail.com>
2017-06-30 10:35:09 +0530

    height balance, height O(logn)

---
+++ trees/avltreemap.py
@@ -0,0 +1,62 @@
+from treemap import TreeMap
+
+
+class AVLTreeMap(TreeMap):
+    "Sorted map implementation using AVL Tree."""
+    # nested _Node class
+    class _Node(TreeMap._Node):
+        "Node class of AVLTreeMap maitains height value for balancing."
+        __slots__ = '_height'
+
+        def __init__(self, element, parent=None, left=None, right=None):
+            super().__init__(element, parent, left, right)
+            self._height = 0  # will be computed during rebalancing
+
+        def left_height(self):
+            return self._left._height if self._left is not None else 0
+
+        def right_height(self):
+            return self._right._height if self._right is not None else 0
+
+    # position base utilities
+    def _recompute_height(self, p):
+        p._node._height = 1 + max(p._node._left_height(),
+                                  p._node._right_height())
+
+    def isbalanced(self, p):
+        return abs(p._node.left_height() - p._node.right_height()) <= 1
+
+    def _tall_child(self, p, favorleft=False):
+        if p._node._left_height() + (1 if favorleft else 0) >\
+                p._node._right_height():
+                    return self.left(p)
+        else:
+            return self.right(p)
+
+    def _tall_grandchild(self, p):
+        child = self._tall_child(p)
+
+        # if child is on left child favorleft child, else favor right
+        alignment = (child == self.left(p))
+        return self._tall_child(child, alignment)
+
+    def _rebalance(self, p):
+        while p is not None:
+            old_height = p._node._height  # trivialy zero if new node
+            if not self.isbalance(p):
+                # perform trinode restructuring setting p to resulting root
+                # and recompute local heights after the restructuring
+                p = self._restructure(self._tall_grandchild(p))
+                self._recompute_height(self.left(p))
+                self._recompute_height(self.right(p))
+            self._recompute_height(p)
+            if p._node._height == old_height:
+                p = None
+            else:
+                p = self.parent(p)
+
+    def _rebalance_insert(self, p):
+        self._rebalance(p)
+
+    def _rebalance_delete(self, p):
+        self._rebalance(p)
